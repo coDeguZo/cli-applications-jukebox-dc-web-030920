4a2628221139b0b4691f7c54360beaa566f19f5f
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-24 23:58:29 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,7 +36,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  if input == list(songs)
+  if input == songs
     puts "Playing #{songs}"
   else
     puts "Invalid input, please try again"
